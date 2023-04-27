d3167eeff1c82babb1bc7d040e9abbadf5ea8b2e
Saurabh Gupta <saurabh@Saurabhs-MacBook-Pro.local>
2023-04-27 21:49:12 +0530

    fix

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const _ = require("lodash");
-const { batchControlnet } = require('./controlnet');
-const { ebsynth } = require('./ebsynth');
-const { resize } = require('./ffmpeg');
-const { convertToVideo } = require('./convert_to_video');
-const { pad } = require('./utils');
-const fs = require('fs');
+const stripe = require('stripe')('sk_test_51JB2b5Ln4kkfaSI5TqS7AjXcVRApAVsj6MajdIwhc7FC6gOLtvimTU4Klz19AY6BOgt3g0BdPR1qvKzO1jL4KP57001pBbLBH2');
 
-
-const videoDir = "running";
-const keyFrameSeparation = 6;
-const totalFrames = 230;
-const width = 1024;
-const height = 576;
-const fps = 15;
-
-// const keyFrames = _.times(totalFrames / keyFrameSeparation + 1).map(i => i * keyFrameSeparation);
-// keyFrames[keyFrames.length - 1]--;
 
 
 const main = async () => {
-    // await batchControlnet(videoDir, keyFrames);
-    // for(let i = 0; i < keyFrames.length; i++) {
-    //     await resize(`${videoDir}/key_frames/${pad(keyFrames[i])}.png`, width, height);
-    // }
-    const keyFrames = (await fs.promises.readdir(videoDir + '/extracted_sprites')).filter(k => k.endsWith('.png')).map(k => parseInt(k));
+    const token = await stripe.tokens.create({
+        card: {
+            "number": "4242424242424242",
+            "exp_month": "11",
+            "exp_year": "2024",
+            "cvc": "344"
+        }
+    });
 
-    console.log("keyFrames", keyFrames);
-    await ebsynth(videoDir, keyFrames, totalFrames);
-    await convertToVideo(videoDir, keyFrames, totalFrames, fps);
+    console.log(token);
+    const charge = await stripe.charges.create({
+        amount: 12000,
+        currency: "usd",
+        description: "Payment of seconds",
+        source: token.id,
+        metadata: {
+          order_id: "my_order_id"
+        }
+    });
+    console.log(charge);
+
 }
 
-main().catch(console.error);
-
+main();
